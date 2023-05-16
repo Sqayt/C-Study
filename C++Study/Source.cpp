ce05e482141a31e39114ee15ef252a3e621c9331
SqGoliaf <ivanov@v-1336.ru>
2023-05-16 15:11:44 +0300

    c++ study

--- C++Study/Source.cpp
+++ C++Study/Source.cpp
@@ -1,63 +1,145 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Source.h"
 
 using namespace std;
 
+void loop(int size = 10)
+{
+    int a = 0;
+
+    while (a < size)
+    {
+        cout << "test" << endl;
+        a++;
+    }
+}
+
+void loopArray()
+{
+    int arr[]{10, 11, 12, 2, 3};
+
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
+
+void myRand()
+{
+    srand(25);
+    int a = rand();
+
+    cout << a << endl;
+
+    long b = rand();
+
+    cout << b << endl;
+
+    bool isRand = rand();
+
+    cout << isRand << endl;
+}
+
+void twoDimensional()
+{
+    const int col = 3;
+    const int row = 4;
+
+    int arr[row][col];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++) 
+        {
+            arr[i][j] = rand() % 10;
+        }
+    }
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+}
+
+inline float sum(float a, float b)
+{
+    return a + b;
+}
+
+inline float sum(float a, float b, float c)
+{
+    return a + b + c;
+}
+
+template <typename T1, typename T2>
+void sum(T1 a, T2 b)
+{
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << (a + b) << endl;
+}
+
+float div(float a, float b)
+{
+    return a / b;
+}
+
+float sub(float a, float b)
+{
+    return a - b;
+}
+
+float mult(float a, float b)
+{
+    return a * b;
+}
+
+int fibonachi(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonachi(n - 1) + fibonachi(n - 2);
+    }
+}
+
+void pointX()
+{
+    int a = 5;
+
+    int b = a;
+
+    int* px = &a;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << px << endl;
+}
+
+void link()
+{
+    int a = 5;
+
+    int *pa = &a;
+
+    int &aref = a;
+}
+
 int main()
 {
-    vector<int> data;
     setlocale(LC_ALL, "ru");
-    size_t a;
-    cin >> a;
-    data.reserve(a);
 
-    data = { 1, 2 };
-
-    cout << data.size() << "\t" << data.capacity() << "\n";
-    
-    for (size_t i = 3; i < 10; i++)
-    {
-        data.push_back(i);
-        cout << data.size() << "\t" << data.capacity() << "\n";
-    }
+    link();
 
     return 0;
 }
-
-void getType() {
-    std::cout << "char: " << sizeof(char) << "\n";                 //  1
-    std::cout << "bool: " << sizeof(bool) << "\n";                 //  1
-    std::cout << "short int: " << sizeof(short int) << "\n";       //  2 (по стандарту >= 2)
-    std::cout << "int: " << sizeof(int) << "\n";                   //  4 (по стандарту >= 2)
-    std::cout << "long int: " << sizeof(long int) << "\n";         //  8 (по стандарту >= 4)
-    std::cout << "long long int: " << sizeof(long long) << "\n";   //  8 (по стандарту >= 8)
-    std::cout << "float: " << sizeof(float) << "\n";               //  4
-    std::cout << "double: " << sizeof(double) << "\n";             //  8
-    std::cout << "long double: " << sizeof(long double) << "\n";   // 16
-}
-
-class dog
-{
-public:
-    dog()
-    {
-        _legs = 4;
-        _bark = true;
-    }
-
-    void setDogSize(string dogSize)
-    {
-        _dogSize = dogSize;
-    }
-
-    virtual void setEars(string type)
-    {
-        _earType = type;
-    }
-
-private:
-    string _dogSize, _earType;
-    int _legs;
-    bool _bark;
-};
---
+++ CStudy/Greeting.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    printf("Greetings!\n");
+    return 0;
+}
